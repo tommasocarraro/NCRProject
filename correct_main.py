98fadcc1ece91eab28deaff4115f83ff0f7a16dc
Tommaso Carraro <tommasocarraro@libero.it>
2021-03-19 16:30:17 +0100

    fixes

--- correct_main.py
+++ correct_main.py
@@ -21,8 +21,6 @@ if __name__ == '__main__':
     raw_dataset = prepare_movielens_100k("datasets/movielens-100k/u.data")
     #raw_dataset = pd.read_csv("datasets/amazon_electronics/electronics.csv")
 
-    save_path = "saved-models/best_movielens_100k.json"
-
     dataset = Dataset(raw_dataset)
 
     dataset.process_data(threshold=4, order=True, leave_n=1, keep_n=5, max_history_length=5)
