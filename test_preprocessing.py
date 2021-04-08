d4193e1e66dbb65615132fef104c5be85f479f4f
bmxitalia <tommasocarraro96@gmail.com>
2021-04-08 18:21:01 +0200

    tested the preprocessing

--- test_preprocessing.py
+++ test_preprocessing.py
@@ -1,10 +1,11 @@
 from ncr.data import Dataset
+import pandas as pd
 
 if __name__ == '__main__':
-    #dataset = Dataset("datasets/u.data")
-    dataset = Dataset("datasets/toy-dataset/fake.csv", sep=',', convert_to_indexes=True)
+    raw_dataset = pd.read_csv("datasets/toy-dataset/fake.csv")
+    dataset = Dataset(raw_dataset)
 
-    dataset.process_data(threshold=4, order=True)
+    dataset.process_data(threshold=4, order=True, leave_n=1, keep_n=5, max_history_length=5, premise_threshold=0)
 
     print("Dataset before preprocessing")
     print(dataset.dataset)
